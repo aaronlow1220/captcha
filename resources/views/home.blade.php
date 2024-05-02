2159e9fd601720f339a1fcf871314b409f964cbb
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-05-03 01:09:14 +0800

    home created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends("layout/layout")
+
+@section("page-title", "首頁")
+
+@section('main-content')
+    <h1>Hello</h1>
+@endsection
---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('page-title') | 我不是機器人</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/nav.css') }}">
+</head>
+<body>
+    @yield("main-content")
+</body>
+</html>
