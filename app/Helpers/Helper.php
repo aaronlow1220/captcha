9f0d89002b53343e0fea179ae6e7db0d714d6265
Aaron Low <117196390+aaronlow1220@users.noreply.github.com>
2024-05-07 11:00:20 +0800

    app

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,8 +1,46 @@
 <?php
+/**
+ * Useful functions through out the application
+ *
+ * @category PHP
+ * @author Aaron Low <aaronlow1220@gmail.com>
+ * @copyright 2024 Aaron
+ *
+ */
 
 namespace App\Helpers;
 
-function formatDate($dateString, $format = 'Y-m-d')
+use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\Auth;
+use App\Models\user;
+
+class Helper
 {
-    
+    // User
+    /**
+     * Generate uuid with custom prefix
+     *
+     * @param string $prefix prefix for generated uuid
+     *
+     * @return string
+     */
+    public static function prefixedUuid($prefix)
+    {
+        return $prefix . ((string) Uuid::uuid4());
+    }
+
+    /**
+     * Generate uuid with custom prefix
+     *
+     * @return bool
+     */
+    public static function isAdmin()
+    {
+        $isAdmin = false;
+        if (Auth::check()) {
+            $isAdmin = user::where("uuid", Auth::user()->uuid)->first()->is_admin;
+        }
+        return $isAdmin;
+    }
+
 }
